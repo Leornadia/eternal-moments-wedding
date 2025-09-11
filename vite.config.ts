4c9aef3f4f4635bfff29ca95cb3c9f63026c4ae7
Leornadia Jose <leornadiajose@gmail.com>
2025-09-11 10:00:45 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/eternal-moments-wedding/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
